29b0497b2c06500314bbd6a6b116da939e8dbaf7
Tyson Trotz <slavatrotz@gmail.com>
2023-09-24 22:17:41 -0500

    add two get request in routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,36 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    if (!categoryData) {
+      res.status(500).json({ message:'Oh no! Something went went wrong. Please try your request again.'});
+      return;
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message:'No catagory with that id found.'});
+      return;
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
